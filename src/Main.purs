c58f5acc4097e91df489d0764158700587c2953d
AJ <z0isch@gmail.com>
2017-09-09 13:07:18 -0400

    made wrapper helper

--- src/Main.purs
+++ src/Main.purs
@@ -5,24 +5,58 @@ import Prelude
 import Control.Coroutine (Consumer, consumer, runProcess, ($$))
 import Control.Monad.Aff (Aff, forkAff)
 import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Monad.Eff.Exception (EXCEPTION, error, throwException)
 import Control.Monad.Eff.Now (NOW)
+import Control.Monad.Error.Class (throwError)
 import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Types (htmlDocumentToDocument)
+import DOM.HTML.Window (document)
+import DOM.Node.Document (createElement, getElementsByClassName, getElementsByTagName)
+import DOM.Node.Element (setId)
+import DOM.Node.HTMLCollection as C
+import DOM.Node.Node (insertBefore, parentElement)
+import DOM.Node.ParentNode (QuerySelector(..))
+import DOM.Node.Types (Element, ElementId(..), Node, elementToNode)
 import DOM.WebStorage.Game (savedGamesProducer)
-import Data.Maybe (Maybe(..))
-import Halogen.Aff (HalogenEffects, awaitBody, runHalogenAff)
+import Data.Maybe (Maybe(..), maybe)
+import Halogen.Aff (HalogenEffects, awaitBody, awaitLoad, runHalogenAff, selectElement)
 import Halogen.VDom.Driver (runUI)
 import Page (Message(..))
 import Page as P
 import Routes as R
 import Routing.Hash (setHash)
 
-main :: Eff (HalogenEffects (now :: NOW, dom :: DOM, console :: CONSOLE )) Unit
+makeWrapper :: forall e. Eff (dom :: DOM | e) (Maybe ElementId)     
+makeWrapper = do
+  htmlDoc <- window >>= document
+  let doc = htmlDocumentToDocument htmlDoc
+  scripts <- getElementsByTagName "script" doc
+  l <- C.length scripts
+  mEl <- map elementToNode <$> C.item (l-1) scripts 
+  case mEl of
+    Nothing -> pure Nothing
+    (Just currScript) -> do
+      mparEl <- parentElement currScript
+      case mparEl of
+        Nothing -> pure Nothing
+        (Just parEl) -> do
+          wrapper <- createElement "div" doc 
+          setId (ElementId "test") wrapper
+          _ <- insertBefore (elementToNode wrapper) currScript (elementToNode parEl)
+          pure (Just $ ElementId "test")
+
+main :: Eff (HalogenEffects (now :: NOW, dom :: DOM, console :: CONSOLE)) Unit
 main = runHalogenAff do
-  body <- awaitBody
-  app <- runUI P.ui unit body
+  mWrapId <- liftEff makeWrapper
+  wrapId <- maybe (throwError (error "Couldn't make wrapper")) pure mWrapId
+  mWrap <- selectElement (QuerySelector "#test")
+  wrap <- maybe (throwError (error "Couldn't find wrapper")) pure mWrap
+  app <- runUI P.ui unit wrap
   _ <- forkAff $ runProcess (R.routeProducer $$ P.routeConsumer app.query)
   _ <- forkAff $ runProcess (savedGamesProducer $$ P.savedGamesConsumer app.query)
   app.subscribe pageMsgConsumer
@@ -33,3 +67,26 @@ pageMsgConsumer = consumer \event -> do
     RouteTo s -> do
       liftEff $ setHash s
       pure Nothing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
